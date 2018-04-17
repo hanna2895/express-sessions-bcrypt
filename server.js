dc1a7e5b0a29cbba542ac6d069627543b213ed90
Hannah Werman <hannah.werman@gmail.com>
2018-04-17 14:51:20 -0500

    added express-session and body-parser

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
 require('./db/db');
 
+// middleware
+app.use(session({
+	secret: 'help I am sleepy af', // used to encrypt cookie, make up a phrase CAREFUL YOU DON'T GET HACKED
+	resave: false,
+	saveUninitialized: false
+})); // session is a function, takes an object
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
 // controllers
 const userController = require('./controllers/userController.js');
 app.use('/users/', userController);
+
+app.get('/', (req, res) => {
+	res.render('links.ejs') // or could also redirect to something that does work		
+})
 
 
 app.get('*', (req, res) => {
